4e45a8e8ab9079d2cac325a338ea85c39e32fd01
Lucas <lucasmendeswow@gmail.com>
2024-10-28 11:53:53 -0300

    aula 011

---
+++ Aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade!`)
+}
+console.log(`Dirija sempre com segurança`)
